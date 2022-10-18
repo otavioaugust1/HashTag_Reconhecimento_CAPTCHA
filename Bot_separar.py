41d14affb49f0c1270f29596cc158a430d8ca857
Otavio Augusto <otavioaugust@gmail.com>
2022-10-18 18:06:40 -0300

    version 1.2

--- Bot_separar.py
+++ Bot_separar.py
@@ -20,12 +20,12 @@ for arquivo in arquivos:
     for contorno in contornos:
         (x, y, largura, altura) = cv2.boundingRect(contorno)
         area = cv2.contourArea(contorno)
-        if area > 20:
+        if area > 18:
             regiao_letras.append((x, y, largura, altura))
 
     # desenhar os contornos
     imagem_final = cv2.merge([imagem] * 3)
-    i = 0
+    i = 1
     for retangulo in regiao_letras:
         x, y, largura, altura = retangulo
         imagem_letra = imagem[y-2:y+altura+2, x-2:x+largura+2]  # colocando margem no retangulo
